b2b11e345a17b5d2e692dd724a0f849ff50df05d
kamily <kamily7803@gmail.com>
2022-07-22 16:41:10 -0300

    feat: interactive image gallery

--- js/script.js
+++ js/script.js
@@ -46,3 +46,21 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// galeria de bicicletas
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const containerGaleria = document.querySelector('.bicicleta-imagens');
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    const media = matchMedia('(min-width: 950px)').matches;
+    if (media) {
+        containerGaleria.prepend(img);
+    }
+}
+
+function eventosGaleria(img) {
+    img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
